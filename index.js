53cb249a5658f8354f98a765c22a4263d66f120f
Vincent Langat <kimutailangat033@gmail.com>
2022-06-07 18:57:35 +0300

    commit tree 6887e7ae970810fc2ab567df2fab7a1b4c963792

--- index.js
+++ index.js
@@ -4,16 +4,25 @@ function saturdayFun(myActivity="roller-skate"){
     return `This Saturday, I want to ${myActivity}!`;
 
 }
+
 function mondayWork(activity="go to the office"){
     return `This Monday, I will ${activity}.`;
 }
-function wrapAdjective(myAdjective="*"){
+// This convention below here worked. Let me try another one below here
+ function wrapAdjective(myAdjective="*"){
     return function ( action="a hardworker"){
-        return `You are ${myAdjective}${action}${myAdjective}!`;
+       return `You are ${myAdjective}${action}${myAdjective}!`;
 
     
     }
 }
+// Here is another method! It has 8 passing tests though 
+// function wrapAdjective(myAdjective="*"){
+//     return function(){
+//         const action ="a hard worker"
+//         return `You are ${myAdjective}${action}${myAdjective}!`;
+//     }
+// }
 
 
 
